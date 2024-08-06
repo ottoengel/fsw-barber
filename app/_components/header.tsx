1b293530c6a41a8eebdd705d6f874afe2927fe43
Otto <ottoengelhardt18@gmail.com>
2024-08-06 14:34:40 -0300

    feat: add BarberShopItem component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
-import { Card, CardContent } from "./card"
-import { Button } from "./button"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
 import { MenuIcon } from "lucide-react"
 
 const Header = () => {
