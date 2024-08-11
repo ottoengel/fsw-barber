1cba3022687293c4f1af08d627acc4803431d1d4
Otto <ottoengelhardt18@gmail.com>
2024-08-11 19:00:04 -0300

    feat: ensure barbershops can be searched by title and service

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -5,19 +5,39 @@ import { db } from "../_lib/prisma"
 
 interface BarberShopsPageProps {
   searchParams: {
-    search?: string
+    title?: string
+    service?: string
   }
 }
 // busca no banco
 const BarbershopsPage = async ({ searchParams }: BarberShopsPageProps) => {
   const barbershops = await db.barbershop.findMany({
     where: {
-      name: {
-        contains: searchParams?.search,
-        mode: "insensitive", //pra nn diferenciar maiuscula e minuscula
-      },
+      OR: [
+        searchParams?.title
+          ? {
+              name: {
+                contains: searchParams?.title,
+                mode: "insensitive", //pra nn diferenciar maiuscula e minuscula
+              },
+            }
+          : {},
+        searchParams.service
+          ? {
+              services: {
+                some: {
+                  name: {
+                    contains: searchParams?.service,
+                    mode: "insensitive",
+                  },
+                },
+              },
+            }
+          : {},
+      ],
     },
   })
+
   return (
     <div>
       <Header />
@@ -26,7 +46,8 @@ const BarbershopsPage = async ({ searchParams }: BarberShopsPageProps) => {
       </div>
       <div className="px-5">
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Resultados para &quot;{searchParams.search}&quot;
+          Resultados para &quot;{searchParams?.title || searchParams?.service}
+          &quot;
         </h2>
         <div className="grid grid-cols-2 gap-4">
           {barbershops.map((barbershop) => (
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import BarbershopItem from "./_components/barbershop-item"
 import { quickSearchOptions } from "./_constants/search"
 import BookingItem from "./_components/booking-item"
 import Search from "./_components/search"
+import Link from "next/link"
 
 const Home = async () => {
   //chamar o banco
@@ -33,14 +34,21 @@ const Home = async () => {
         {/* BUSCA RAPIDA */}
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
           {quickSearchOptions.map((option) => (
-            <Button className="gap-2" variant="secondary" key={option.title}>
-              <Image
-                src={option.imageUrl}
-                width={16}
-                height={16}
-                alt={option.title}
-              />
-              {option.title}
+            <Button
+              className="gap-2"
+              variant="secondary"
+              key={option.title}
+              asChild
+            >
+              <Link href={`/barbershops?service=${option.title}`}>
+                <Image
+                  src={option.imageUrl}
+                  width={16}
+                  height={16}
+                  alt={option.title}
+                />
+                {option.title}
+              </Link>
             </Button>
           ))}
         </div>
