116823441ad2a97fcc1cd2173720180a655c72c8
Otto <ottoengelhardt18@gmail.com>
2024-08-24 15:14:43 -0300

    chore: update images

--- app/page.tsx
+++ app/page.tsx
@@ -94,7 +94,7 @@ const Home = async () => {
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
-            src="/banner-01.png"
+            src="/banner-01.png" // /banner-01.png
             fill
             className="rounded-xl object-cover"
           />
